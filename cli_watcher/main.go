07cba76197f62fcbebaa3afc55f7883642a79e99
samuelorozco <samuelorozco@clearwateranalytics.com>
2020-11-30 11:02:24 -0700

    Making a better cli for cliw

--- cli_watcher/main.go
+++ cli_watcher/main.go
@@ -3,28 +3,50 @@ package main
 import (
 	"fmt"
 	"github.com/gen2brain/beeep"
+	"github.com/spf13/cobra"
 	"os"
 	"os/exec"
 	"strings"
 )
 
-func main() {
+var (
+	rootCmd = &cobra.Command{
+		Use:   "cliw",
+		Short: "Cli Watcher",
+		Long:  `Watch your cli commands`,
+		Run: func(cmd *cobra.Command, args []string) {
+			osArgsLen := len(os.Args)
+			if osArgsLen < 2 {
+				println("no arguments passed")
+				os.Exit(0)
+			}
 
-	osArgsLen := len(os.Args)
-	if osArgsLen < 2 {
-		println("no arguments passed")
-		os.Exit(0)
+			newArgs := os.Args[1:]
+			RunWatcher(newArgs)
+		},
 	}
+)
 
-	newArgs := os.Args[1:]
-	commandString := strings.Join(newArgs, " ")
-	cmd := exec.Command(newArgs[0], newArgs[1:]...)
+func main() {
+	Execute()
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		panic(err)
+	}
+}
+
+func RunWatcher(args []string) {
+	commandString := strings.Join(args, " ")
+	cmd := exec.Command(args[0], args[1:]...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
 
 	err := cmd.Run()
 	if err != nil {
-		err = beeep.Notify("Error", fmt.Sprintf("There was an error running (%s)", commandString), "assets/warning.png")
+		err = beeep.Alert("Error", fmt.Sprintf("There was an error running (%s)", commandString), "assets/warning.png")
 		if err != nil {
 			panic(err)
 		}
@@ -32,7 +54,7 @@ func main() {
 	}
 
 	// notify
-	err = beeep.Notify("Finished", fmt.Sprintf("(%s) has finished running", commandString), "assets/information.png")
+	err = beeep.Alert("Finished", fmt.Sprintf("(%s) has finished running", commandString), "assets/information.png")
 	if err != nil {
 		panic(err)
 	}
