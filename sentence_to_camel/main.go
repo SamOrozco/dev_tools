9aae5ea7e9337e5e89a6a7fe014caa3bd0bb352d
samuelorozco <samuelorozco@clearwateranalytics.com>
2020-12-18 16:40:44 -0700

    adding ability to camel case sentences

---
+++ sentence_to_camel/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"os"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		panic("no string passed")
+	}
+	sentence := os.Args[1]
+	println(convertSentenceToCamelCase(sentence))
+}
+
+func convertSentenceToCamelCase(sent string) string {
+	segments := strings.Fields(sent)
+	bldr := strings.Builder{}
+	for i := range segments {
+		currentSeg := segments[i]
+		if i != 0 {
+			currentSeg = strToUpper(currentSeg)
+		}
+		bldr.WriteString(currentSeg)
+	}
+	return bldr.String()
+}
+
+func strToUpper(val string) string {
+	if len(val) == 1 {
+		return strings.ToUpper(val)
+	}
+	return strings.ToUpper(val[0:]) + val[1:]
+}
